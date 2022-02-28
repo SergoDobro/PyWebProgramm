de17e71e48f31f758adbb3ae9872478bf94de323
SergoDobro <optimgrey@yandex.ru>
2022-02-28 20:39:47 +0300

    Optimised code with incapsulations

--- main.py
+++ main.py
@@ -2,6 +2,27 @@ import uvicorn
 from fastapi import FastAPI, Body
 from fastapi.responses import PlainTextResponse
 import sqlite3
+from enum import Enum, auto
+
+class DBAction(Enum):
+    fetchone = auto()
+    fetchall = auto()
+    commit = auto()
+
+def db_action(sql: str, args: tuple, action: DBAction):
+    conn = sqlite3.connect('db.sqlite')
+    cursor = conn.cursor()
+    cursor.execute(sql, args)
+    if action == DBAction.fetchone:
+        result = cursor.fetchone()
+    elif action == DBAction.fetchall:
+        result = cursor.fetchall()
+    elif action == DBAction.commit:
+        conn.commit()
+        result = None
+    cursor.close()
+    conn.close()
+    return result
 
 app = FastAPI()
 
@@ -28,28 +49,20 @@ def MainPageIntro():
 
 @app.post('/login')
 def login(username: str = Body(...), password: str = Body(...)):
-    conn = sqlite3.connect('db.sqlite')
-    cursor = conn.cursor()
-    cursor.execute('''
-        select * from users where username = ? and password = ?
-    ''', (username, password))
-    user = cursor.fetchone()
-    print(username+' '+password)
-    print(user)
-    cursor.close()
-    conn.close()
-    return user
 
-@app.post('/test')
-def test():
-    conn = sqlite3.connect('db.sqlite')
-    cursor = conn.cursor()
-    cursor.execute('''
-        insert into users (username, password) values ('test', 'password')
-    ''')
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return None
+    user = db_action('''
+        select * from users where username = ? and password = ?
+    ''', (username, password), DBAction.fetchone)
+    if user == None:
+        return "Failed to log in"
+    return "Successfully logged in"
+
+@app.post('/redister')
+def test(username: str = Body(...), password: str = Body(...)):
+
+    res = db_action('''
+        insert into users (username, password) values (?, ?)
+    ''', (username, password), DBAction.commit)
+    return "Success"
 
 uvicorn.run(app)
