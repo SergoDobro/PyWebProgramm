989e2d7aaf26389653e48cb4292046a6287bc950
SergoDobro <optimgrey@yandex.ru>
2022-04-04 19:46:08 +0300

    The great return after weeks...

--- main.py
+++ main.py
@@ -1,74 +1,149 @@
-import uvicorn
-from fastapi import FastAPI, Body
-from fastapi.responses import PlainTextResponse
 import sqlite3
-from enum import Enum, auto
 
-class DBAction(Enum):
-    fetchone = auto()
-    fetchall = auto()
-    commit = auto()
+import jose.exceptions
+import uvicorn
+from fastapi import FastAPI, Body, Header, Depends
+from fastapi.exceptions import HTTPException
+from fastapi.responses import HTMLResponse
+from jose import jwt
 
-def db_action(sql: str, args: tuple, action: DBAction):
-    conn = sqlite3.connect('db.sqlite')
-    cursor = conn.cursor()
-    cursor.execute(sql, args)
-    if action == DBAction.fetchone:
-        result = cursor.fetchone()
-    elif action == DBAction.fetchall:
-        result = cursor.fetchall()
-    elif action == DBAction.commit:
-        conn.commit()
-        result = None
-    cursor.close()
-    conn.close()
-    return result
+import config
+from utils import db_action, DBAction, run_code
+from task_checker import get_task
 
 app = FastAPI()
-
 
 
 @app.on_event('startup')
 def create_db():
     conn = sqlite3.connect('db.sqlite')
     cursor = conn.cursor()
+
     cursor.execute('''
         create table if not exists users (
-        id integer primary_key,
-        username varchar not null,
-        password varchar not null
+            id integer primary key,
+            username varchar not null,
+            password varchar not null
         );
     ''')
+    cursor.execute('''
+        create table if not exists tasks (
+            id integer primary key,
+            name varchar not null,
+            description varchar,
+            output varchar not null
+        );
+    ''')
+
     cursor.close()
+    conn.close()
+
+
+def get_user(authorization: str = Header(...)):
+    try:
+        user_id = jwt.decode(authorization, config.SECRET, algorithms=['HS256'])['id']
+    except jose.exceptions.JWTError:
+        raise HTTPException(
+            status_code=400,
+            detail='Неверный токен'
+        )
+
+    user = db_action(
+        '''
+            select * from users where id = ?
+        ''',
+        (user_id,),
+        DBAction.fetchone,
+    )
+    return user
+
+
+def send_html(name: str):
+    with open(f'html/{name}.html', 'r', encoding='utf-8') as f:
+        return HTMLResponse(f.read())
 
 
 @app.get('/')
-def MainPageIntro():
-    return PlainTextResponse("Welcome!/nHave a nice day!")
+def index():
+    return send_html('index')
 
 
-@app.post('/login')
+@app.get('/login')
+def login_page():
+    return send_html('login')
+
+
+@app.get('/register')
+def register_page():
+    return send_html('register')
+
+
+@app.get('/api/ping')
+def ping(user: list = Depends(get_user)):
+    return {
+        'response': 'Pong',
+        'username': user[1],
+    }
+
+
+@app.post('/api/execute')
+def execute(
+    user: list = Depends(get_user),
+    code: str = Body(..., embed=True),
+):
+    return {
+        'result': run_code(code),
+    }
+
+
+@app.post('/api/login')
 def login(username: str = Body(...), password: str = Body(...)):
+    user = db_action(
+        '''
+            select * from users where username = ? and password = ?
+        ''',
+        (username, password),
+        DBAction.fetchone,
+    )
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail='Пользователь не найден'
+        )
 
-    user = db_action('''
-        select * from users where username = ? and password = ?
-    ''', (username, password), DBAction.fetchone)
-    if user == None:
-        return "Failed to log in"
-    return "Successfully logged in"
+    token = jwt.encode({'id': user[0]}, config.SECRET, algorithm='HS256')
+    return {
+        'token': token
+    }
 
-@app.post('/register')
-def test(username: str = Body(...), password: str = Body(...)):
 
-    user = db_action('''
-        select * from users where username = ?
-    ''', (username, ), DBAction.fetchone)
-    if user != None:
-        return "Failed to register. This user does already exist."
+@app.post('/api/register')
+def register(username: str = Body(...), password: str = Body(...)):
+    user = db_action(
+        '''
+            select * from users where username = ?
+        ''',
+        (username,),
+        DBAction.fetchone,
+    )
+    if user:
+        raise HTTPException(
+            status_code=400,
+            detail='Пользователь уже существует'
+        )
 
-    res = db_action('''
-        insert into users (username, password) values (?, ?)
-    ''', (username, password), DBAction.commit)
-    return "Successfully registered"
+    db_action(
+        '''
+            insert into users (username, password) values (?, ?)
+        ''',
+        (username, password),
+        DBAction.commit,
+    )
 
-uvicorn.run(app)
+    return {
+        'message': 'Успешная регистрация'
+    }
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', reload=True)
